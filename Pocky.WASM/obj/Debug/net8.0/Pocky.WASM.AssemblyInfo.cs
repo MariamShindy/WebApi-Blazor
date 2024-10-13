ee05ce64b0b2d07f45bab3a8d3a49ec9bed0f934
MariamShindy <125688137+MariamShindy@users.noreply.github.com>
2024-10-13 13:01:08 +0300

    adding ui for home page

--- Pocky.WASM/obj/Debug/net8.0/Pocky.WASM.AssemblyInfo.cs
+++ Pocky.WASM/obj/Debug/net8.0/Pocky.WASM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pocky.WASM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+560c2466bdddb9b29b5e0cea0e546c8a68891ac9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pocky.WASM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pocky.WASM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
